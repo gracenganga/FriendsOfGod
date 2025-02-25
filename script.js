6e6f2ce81c44217d4c47c57981796d275643716e
gracenganga <ngangagrace868@gmail.com>
2025-02-25 20:50:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ document.getElementById("contact-form").addEventListener("submit", function(even
     document.getElementById("form-response").style.display = "block";
     this.reset(); // Clears the form after submission
 });
+
+// Testimony Submission Handling
+document.getElementById("faith-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent actual submission
+
+    // Show success message
+    document.getElementById("faith-response").style.display = "block";
+
+    // Clear the form
+    this.reset();
+});
